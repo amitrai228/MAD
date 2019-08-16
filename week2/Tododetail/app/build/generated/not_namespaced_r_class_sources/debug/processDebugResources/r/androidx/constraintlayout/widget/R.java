c0b5becab6cf9118f8e94d720dd6e4ef64b8235c
Amit Rai <amitrai228@gmail.com>
2019-08-16 09:36:48 +0545

    message

--- week2/Tododetail/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
+++ week2/Tododetail/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080021;
-        public static final int end = 0x7f080036;
-        public static final int gone = 0x7f08003d;
-        public static final int invisible = 0x7f080047;
-        public static final int left = 0x7f080049;
-        public static final int packed = 0x7f080058;
-        public static final int parent = 0x7f080059;
-        public static final int percent = 0x7f08005b;
-        public static final int right = 0x7f080061;
-        public static final int spread = 0x7f080079;
-        public static final int spread_inside = 0x7f08007a;
-        public static final int start = 0x7f08007f;
-        public static final int top = 0x7f08008e;
-        public static final int wrap = 0x7f080094;
+        public static final int end = 0x7f080038;
+        public static final int gone = 0x7f08003f;
+        public static final int invisible = 0x7f080049;
+        public static final int left = 0x7f08004b;
+        public static final int packed = 0x7f08005a;
+        public static final int parent = 0x7f08005b;
+        public static final int percent = 0x7f08005d;
+        public static final int right = 0x7f080063;
+        public static final int spread = 0x7f08007b;
+        public static final int spread_inside = 0x7f08007c;
+        public static final int start = 0x7f080081;
+        public static final int top = 0x7f080090;
+        public static final int wrap = 0x7f080096;
     }
     public static final class styleable {
         private styleable() {}
---
+++ week2/Tododetail/app/src/main/java/com/example/todo_detail/todo_detail.java
@@ -0,0 +1,19 @@
+package com.example.todo_detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class todo_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_detail);
+
+        //Intent intent = getIntent();
+        //intent.getIntExtra (MainActivity.TodoIndex,0);
+        //textView = findViewById(R.id.textView);
+    }
+}
